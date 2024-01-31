70d74e30334912cc35d0631aafb310978306b406
TaurusTheBull <103037786+FourOFour@users.noreply.github.com>
2024-01-31 19:43:16 +0330

    login1
    polished up and added first sample login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Sidebar from './components/Sidebar/Sidebar'
+import Login1 from './components/logins/login-1'
 
 function App() {
   return (
-    <>
+    <div className='w-full'>
       <Sidebar />
-    </>
+      <div className='relative md:ml-64'>
+        <Login1 />
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
