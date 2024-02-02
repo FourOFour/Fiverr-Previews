a2302bb8bf21980fdda7bf0eeabbe50fbf95e6a2
TaurusTheBull <103037786+FourOFour@users.noreply.github.com>
2024-02-02 15:21:36 +0330

    Logos & Sidebar
    - change logo
    - overall sidebar
    - title

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
-import Login1 from "./components/logins/login-1";
+import SignInUp1 from "./components/sign-in-up/sign-in-up-1";
 import { useState } from "react";
 
 function App() {
     const [isDark, setIsDark] = useState(false);
 
-    function toggleDark() {
+    function toggleIsDark() {
         setIsDark(!isDark);
     }
 
     return (
-        <div className={"w-full" + (isDark ? " dark" : "")}>
-            <Sidebar toggleDark={toggleDark} />
-            <div className="relative md:ml-64">
-                <Login1 />
-            </div>
+        <div className={"w-full h-full" + (isDark ? " dark" : "")}>
+            <aside className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 dark:bg-gray-900 border-r-2 border-gray-300 dark:border-gray-600 shadow-r dark:shadow-gray-600 shadow-inner shadow-lg">
+                <Sidebar isDark={isDark} toggleIsDark={toggleIsDark} />
+            </aside>
+            <main className="relative md:ml-64">
+                <SignInUp1 />
+            </main>
         </div>
     );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,178 @@
 import "./Sidebar.css";
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 function Sidebar(props) {
     return (
         <>
-            <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
-                <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
-                    <button onClick={() => props.toggleDark()}>
-                        Toggle dark
+            <div className="flex flex-col dark:text-white">
+                <header className="p-4 flex justify-between">
+                    <h1 className="text-xl font-bold">
+                        My App
+                    </h1>
+                    <button
+                        type="button"
+                        aria-pressed="false"
+                        data-state="off"
+                        className="items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground bg-transparent h-10 px-3 -mt-1.5"
+                        aria-label="Toggle theme"
+                        onClick={props.toggleIsDark}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className={"h-4 w-4 " + (props.isDark ? '' : 'hidden')}
+                        >
+                            <circle cx="12" cy="12" r="4"></circle>
+                            <path d="M12 2v2"></path>
+                            <path d="M12 20v2"></path>
+                            <path d="m4.93 4.93 1.41 1.41"></path>
+                            <path d="m17.66 17.66 1.41 1.41"></path>
+                            <path d="M2 12h2"></path>
+                            <path d="M20 12h2"></path>
+                            <path d="m6.34 17.66-1.41 1.41"></path>
+                            <path d="m19.07 4.93-1.41 1.41"></path>
+                        </svg>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className={"h-4 w-4 " + (props.isDark ? 'hidden' : '')}
+                        >
+                            <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+                        </svg>
                     </button>
+                </header>
+                <div className="flex flex-row">
+                    <div className="w-64">
+                        <nav className="mt-4">
+                            <ul>
+                                <li className="mb-2">
+                                    <a
+                                        className="flex items-center py-2 px-4 hover:bg-gray-300 dark:hover:bg-gray-700"
+                                        href="#"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            className="h-6 w-6"
+                                        >
+                                            <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                                        </svg>
+                                        <span className="ml-2">Home</span>
+                                    </a>
+                                </li>
+                                <li className="mb-2">
+                                    <a
+                                        className="flex items-center py-2 px-4 hover:bg-gray-300 dark:hover:bg-gray-700"
+                                        href="#"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            className="h-6 w-6"
+                                        >
+                                            <path d="M12 2L12 22"></path>
+                                            <path d="M12 6L18 6"></path>
+                                        </svg>
+                                        <span className="ml-2">Logo</span>
+                                    </a>
+                                </li>
+                                <li className="mb-2">
+                                    <a
+                                        className="flex items-center py-2 px-4 hover:bg-gray-300 dark:hover:bg-gray-700"
+                                        href="#"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            className="h-6 w-6"
+                                        >
+                                            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                                            <circle
+                                                cx="12"
+                                                cy="7"
+                                                r="4"
+                                            ></circle>
+                                        </svg>
+                                        <span className="ml-2">
+                                            Profile
+                                        </span>
+                                    </a>
+                                </li>
+                                <li className="mb-2">
+                                    <a
+                                        className="flex items-center py-2 px-4 hover:bg-gray-300 dark:hover:bg-gray-700"
+                                        href="#"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            className="h-6 w-6"
+                                        >
+                                            <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path>
+                                            <circle
+                                                cx="12"
+                                                cy="12"
+                                                r="3"
+                                            ></circle>
+                                        </svg>
+                                        <span className="ml-2">
+                                            Settings
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
-            </nav>
+            </div>
         </>
     );
 }
 
 Sidebar.propTypes = {
-    toggleDark: PropTypes.func.isRequired,
-};
+    toggleIsDark: PropTypes.func,
+    isDark: PropTypes.bool
+}
 
 export default Sidebar;
