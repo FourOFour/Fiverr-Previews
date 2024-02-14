90edf8f00a071ecc1b256f448362693a97e5e030
TaurusTheBull <103037786+FourOFour@users.noreply.github.com>
2024-02-14 19:47:43 +0330

    Responsive & clean up
    - responsive related fixes and patches
    - some clean up and patches

--- src/app-1.jsx
+++ src/app-1.jsx
@@ -14,7 +14,7 @@ function App() {
 
     return (
         <div className={"w-full h-full" + (isDark ? " dark" : "")}>
-            <aside className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:overflow-hidden shadow-xl bg-white relative md:w-48 z-10 dark:bg-gray-900 border-r-2 border-gray-300 dark:border-gray-600 shadow-r dark:shadow-gray-600 shadow-inner shadow-lg">
+            <aside className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:overflow-hidden md:shadow-xl bg-white relative md:w-48 z-10 dark:bg-gray-900 md:border-r-2 md:border-gray-300 dark:border-gray-600 md:shadow-r dark:shadow-gray-600 md:shadow-inner md:shadow-lg">
                 <Sidebar isDark={isDark} toggleIsDark={toggleIsDark} />
             </aside>
             <main className="relative md:ml-48">
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -5,7 +5,7 @@ import FacebookIcon from "../../assets/svg/icons/facebook.svg?react";
 function Hero() {
     return (
         <>
-            <section className="relative w-full min-h-[400px] grid items:center dark:text-white text-black">
+            <section className="relative w-full min-h-[400px] sm:grid items:center dark:text-white text-black px-4 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 dark:from-gray-700 dark:via-gray-800 dark:to-gray-900 pb-10">
                 <img
                     alt="Hero"
                     className="absolute inset-0 object-cover w-full h-full bg-cover z-[1] dark:block hidden"
@@ -16,12 +16,12 @@ function Hero() {
                     className="absolute inset-0 object-cover w-full h-full bg-cover z-[1] dark:hidden"
                     src={import.meta.env.BASE_URL + "images/hero1_light.jpg"}
                 />
-                <div className="container px-4 text-center md:px-6 lg:text-left relative z-10 h-screen md:max-w-[50%]">
-                    <div className="mb-[5vh]">
-                        <p className="text-xs font-medium tracking-wider uppercase mt-[5vh] mb-[13vh]">
+                <div className="container px-1 sm:px-4 text-center md:px-6 lg:text-left relative z-10 min-h-screen lg:max-w-[50%] md:max-w-[75%] mr-auto ml-auto md:ml-0">
+                    <div className="sm:mb-[5vh] mb-5">
+                        <p className="text-xs font-medium tracking-wider uppercase sm:pt-[5vh] pt-5 sm:mb-[13vh] mb-10">
                             Always up to date
                         </p>
-                        <h1 className="text-3xl font-bold tracking-tighter lg:text-4xl/none mb-[4vh]">
+                        <h1 className="text-3xl font-bold tracking-tighter lg:text-4xl/none sm:mb-[4vh] mb-4">
                             The future of frontend development
                         </h1>
                         <p className="text-sm">
@@ -30,24 +30,24 @@ function Hero() {
                             latest news and features. No spam.
                         </p>
                     </div>
-                    <div className="mb-[1vh]">
+                    <div className="sm:mb-[1vh] mb-1">
                         <input
                             type="text"
-                            className="h-10 border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full block rounded-full"
+                            className="h-10 border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full rounded-full"
                             placeholder="Your first and last name"
                         />
                     </div>
-                    <div className="flex flex-row gap-1 w-full mb-[4vh]">
+                    <div className="flex md:flex-row flex-col gap-1 w-full sm:mb-[4vh] mb-4">
                         <input
                             type="email"
-                            className="h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-l-full flex-auto"
+                            className="h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-l-full rounded-r-full md:rounded-r-none flex-auto"
                             placeholder="Enter your email"
                         />
-                        <button className="inline-flex items-center justify-center flex-none w-32 whitespace-nowrap rounded-md font-medium transition-colors rounded-r-full md:translate-x-2 ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 py-2 px-4 text-center bg-blue-800 hover:bg-blue-700 rounded-md text-white text-sm focus:outline-none uppercase">
+                        <button className="inline-flex items-center justify-center flex-none md:w-32 whitespace-nowrap rounded-md font-medium transition-colors rounded-r-full rounded-l-full md:rounded-l-none md:translate-x-2 ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 py-2 px-4 text-center bg-blue-800 hover:bg-blue-700 rounded-md text-white text-sm focus:outline-none uppercase">
                             Yes, I&apos;m in
                         </button>
                     </div>
-                    <div className="">
+                    <div className="mb-5 text-center md:text-left">
                         <button className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground rounded-full m-1 bg-blue-600 hover:bg-blue-500 text-white w-8 h-8 p-2 border-blue-800 dark:border-blue-300">
                             <TwitterIcon />
                             <span className="sr-only">Twitter</span>
@@ -61,7 +61,7 @@ function Hero() {
                             <span className="sr-only">Facebook</span>
                         </button>
                     </div>
-                    <div className="md:absolute bottom-[2vh] left-[5vh]">
+                    <div className="md:absolute md:bottom-[2vh] md:left-[5vh]">
                         <p className="text-xs font-medium tracking-wider uppercase">
                             &copy;2024 KarabonTM, All Rights Reserved.
                         </p>
