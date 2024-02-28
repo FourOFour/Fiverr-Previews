b96455ba4855899ced524bf645e26302ec9b8f08
TaurusTheBull <103037786+FourOFour@users.noreply.github.com>
2024-02-28 19:26:16 +0330

    toggle/navbar1
    - toggle component
    - navbar1 responsive + using toggle comp.

--- src/components/layouts/navbar/navbar-1.tsx
+++ src/components/layouts/navbar/navbar-1.tsx
@@ -1,12 +1,13 @@
 import PackageIcon from "../../../assets/svg/icons/package.svg";
 import KarabonLogo from "../../../assets/svg/logo/karabon.svg";
 import ArrowDownIcon from "../../../assets/svg/icons/arrow-down.svg";
+import Toggle, { ToggleBody, ToggleButton } from "../../toggle/toggle";
 
 function Navbar1() {
     return (
         <>
-            <nav className="flex flex-row items-center md:h-14 px-4 border-b-[1px] gap-4 md:gap-6 text-black dark:text-white bg-white dark:bg-gray-900 border-gray-300 dark:border-gray-600">
-                <div className="flex items-center space-x-2 lg:space-x-4">
+            <nav className="sm:flex sm:flex-row sm:items-center md:h-14 px-4 border-b-[1px] gap-4 md:gap-6 text-black dark:text-white bg-white dark:bg-gray-900 border-gray-300 dark:border-gray-600">
+                <div className="sm:flex sm:items-center sm:space-x-2 lg:space-x-4">
                     <a
                         className="flex items-center space-x-2 text-lg font-bold"
                         href="#"
@@ -23,41 +24,60 @@ function Navbar1() {
                         />
                     </a>
                 </div>
-                <div className="ml-auto flex items-center space-x-4">
+                <div className="ml-auto sm:flex sm:items-center sm:space-x-4">
                     <a className="text-sm font-medium hover:underline" href="#">
                         Documentation
                     </a>
                     <div className="relative">
-                        <a
-                            className="text-sm font-medium hover:underline"
-                            href="#"
-                        >
-                            Pricing
-                        </a>
-                        <div className="absolute w-48 top-0 right-0 hidden">
-                            <div>Basic</div>
-                            <div>Pro</div>
-                            <div>Enterprise</div>
-                        </div>
+                        <Toggle fade={true} fadeTimer={2500}>
+                            <ToggleButton className="text-sm font-medium hover:underline">
+                                Pricing
+                                <ArrowDownIcon
+                                    className="inline-block ml-1"
+                                    height={10}
+                                    width={10}
+                                />
+                            </ToggleButton>
+                            <ToggleBody>
+                                <div className="sm:absolute sm:w-48 sm:top-[100%] sm:right-0 text-black dark:text-white sm:bg-white sm:dark:bg-gray-900 sm:border-gray-300 sm:dark:border-gray-600 sm:border-[1px] px-3 py-2 sm:rounded">
+                                    <a
+                                        className="text-sm font-medium hover:underline block"
+                                        href="#"
+                                    >
+                                        Basic
+                                    </a>
+                                    <a
+                                        className="text-sm font-medium hover:underline block"
+                                        href="#"
+                                    >
+                                        Pro
+                                    </a>
+                                    <a
+                                        className="text-sm font-medium hover:underline block"
+                                        href="#"
+                                    >
+                                        Enterprise
+                                    </a>
+                                </div>
+                            </ToggleBody>
+                        </Toggle>
                     </div>
                     <a className="text-sm font-medium hover:underline" href="#">
                         Contact Sales
                     </a>
                     <div className="relative">
-                        <a
-                            className="inline-flex h-8 w-8 rounded-full border border-gray-200 items-center justify-center overflow-hidden ring-gray-900/40 dark:ring-gray-50/40 hover:ring-[1px]"
-                            href="#"
-                        >
-                            <ArrowDownIcon
-                                className="rounded-full"
-                                height={16}
-                                width={16}
-                                style={{
-                                    aspectRatio: "16/16",
-                                    objectFit: "cover",
-                                }}
-                            />
-                            {/* <img
+                        <Toggle fade={true} fadeTimer={2500}>
+                            <ToggleButton className="inline-flex h-8 w-8 rounded-full border border-gray-200 items-center justify-center overflow-hidden ring-gray-900/40 dark:ring-gray-50/40 hover:ring-[1px]">
+                                <ArrowDownIcon
+                                    className="rounded-full"
+                                    height={16}
+                                    width={16}
+                                    style={{
+                                        aspectRatio: "16/16",
+                                        objectFit: "cover",
+                                    }}
+                                />
+                                {/* <img
                                 alt="Avatar"
                                 className="rounded-full"
                                 height={32}
@@ -68,15 +88,33 @@ function Navbar1() {
                                     objectFit: "cover",
                                 }}
                             /> */}
-                            <span className="sr-only">
-                                Toggle navigation menu
-                            </span>
-                        </a>
-                        <div className="absolute w-48 hidden">
-                            <div>Basic</div>
-                            <div>Pro</div>
-                            <div>Enterprise</div>
-                        </div>
+                                <span className="sr-only">
+                                    Toggle navigation menu
+                                </span>
+                            </ToggleButton>
+                            <ToggleBody>
+                                <div className="sm:absolute sm:w-48 sm:top-[100%] sm:right-0 text-black dark:text-white sm:bg-white sm:dark:bg-gray-900 sm:border-gray-300 sm:dark:border-gray-600 sm:border-[1px] px-3 py-2 sm:rounded">
+                                    <a
+                                        className="text-sm font-medium hover:underline block"
+                                        href="#"
+                                    >
+                                        Basic
+                                    </a>
+                                    <a
+                                        className="text-sm font-medium hover:underline block"
+                                        href="#"
+                                    >
+                                        Pro
+                                    </a>
+                                    <a
+                                        className="text-sm font-medium hover:underline block"
+                                        href="#"
+                                    >
+                                        Enterprise
+                                    </a>
+                                </div>
+                            </ToggleBody>
+                        </Toggle>
                     </div>
                 </div>
             </nav>
---
+++ src/components/toggle/toggle.tsx
@@ -0,0 +1,54 @@
+import { createContext, useContext, useState } from "react";
+import { ToggleBodyProps, ToggleButtonProps, ToggleContextType, ToggleProps } from "./toggle.interface";
+
+const ToggleContext = createContext<ToggleContextType | null>(null);
+
+function Toggle(props: ToggleProps) {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    return (
+        <>
+            <ToggleContext.Provider value={{isOpen, setIsOpen, fade: (props.fade ? props.fade: false ), fadeTimer: (props.fadeTimer ? props.fadeTimer : 2000)}}>
+                {props.children}
+            </ToggleContext.Provider>
+        </>
+    )
+}
+
+function ToggleButton(props:ToggleButtonProps) {
+    const {isOpen, setIsOpen} = useContext(ToggleContext) as ToggleContextType;
+    
+    return (
+        <>
+            <button className={props.className} onClick={() => setIsOpen(!isOpen)}>
+                {props.children}
+            </button>
+        </>
+    )
+}
+
+function ToggleBody(props:ToggleBodyProps) {
+    const {isOpen, setIsOpen, fade, fadeTimer} = useContext(ToggleContext) as ToggleContextType;
+    let TimeOutTimer = -1;
+    function setTimer() {
+        if (fade)
+            TimeOutTimer = setTimeout(function() {
+                setIsOpen(!isOpen);
+            }, fadeTimer)
+    }
+
+    function resetTimer() {
+        if (fade)
+            clearTimeout(TimeOutTimer);
+    }    
+
+    return (
+        <>
+            <div onMouseLeave={setTimer} onMouseEnter={resetTimer}>
+                {isOpen && props.children}
+            </div>
+        </>
+    )
+}
+
+export default Toggle;
+export {ToggleButton, ToggleBody}
